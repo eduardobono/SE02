8c2c832411ba9af58a6f479879e1cda470954354
eduardobono <eduardo.bono@pfsgroup.es>
2022-03-02 18:11:40 +0100

    Ejercicios

---
+++ Ejercicios 2-03/Ejercicio3.py
@@ -0,0 +1,36 @@
+'''
+        ###########################
+        ### Autor: Eduardo Bono ###
+        ###  Fecha: 02/03/2022  ###
+        ###     Ejercicio 3     ###
+        ###########################
+'''
+
+# ANOTACIONES
+
+
+# VARIABLES
+X = 4
+Y = 5
+N = 2.4
+M = 4.8
+
+# FUNCIONES
+
+
+print("La suma de X e Y es: ", X + Y)
+print("La diferencia entre Y e X es: ", Y - X)
+print("El producto de X e Y es: ", X * Y)
+print("La cociente de X entre Y es: ", X / Y)
+print("El resto de X e Y es: ", X % Y)
+print("La suma de N y M es: %.4f" % (X + Y))
+print("La diferrencia entre N y M es: %.4f" % (X + M))
+print("El producto de N y M es: %.4f" % (X * M))
+print("El cociente de N entre M es: %.4f" % (X / M))
+print("El resto de N entre M es: %.4f" % (X % M))
+print("La suma de X y N es:  %.4f" % (X + N))
+print("El cociente de Y entre M es: %.4f" % (Y / M))
+print("El resto de Y entre M es: %.4f" % (Y % M))
+print("El doble de las variables es: X = ", X * 2, ", Y = ", Y * 2, ", N = %.4f" % (N * 2), ", M = %.4f" % (M * 2))
+print("La suma de todas las variables es: %.4f" % (X + Y + N + M))
+print("El producto de todas las variables es: %.4f" % (X * Y * N * M))
---
+++ Ejercicios 2-03/Ejercicio4.py
@@ -0,0 +1,21 @@
+'''
+        ###########################
+        ### Autor: Eduardo Bono ###
+        ###  Fecha: 02/03/2022  ###
+        ###     Ejercicio 4     ###
+        ###########################
+'''
+
+# ANOTACIONES
+
+
+# VARIABLES
+N = 14
+
+# FUNCIONES
+
+
+print("El valor de N es: ", N)
+print("Incrementar N en 77: ", (N + 77))
+print("Decrementarla en 3: ", (N - 3))
+print("Duplicar su valor: ", (N *2))
