47047c9132ce6f22d5ce006585dd8d0a7aa0c286
eduardobono <eduardo.bono@pfsgroup.es>
2022-03-08 16:29:19 +0100

    subiendo

---
+++ Ejercicio16.py
@@ -0,0 +1,18 @@
+'''
+###########################
+### Autor: Eduardo Bono ###
+###  Fecha: 02/03/2022  ###
+###     Ejercicio 16    ###
+###########################
+'''
+
+
+# ANOTACIONES
+
+
+# VARIABLES
+
+
+# FUNCIONES
+
+
