ea0d2870039a0148b30c3806d5ee09552ad8a095
eduardobono <eduardo.bono@pfsgroup.es>
2022-03-08 13:34:35 +0100

    subiendo

---
+++ Ejercicio2.py
@@ -0,0 +1,27 @@
+'''
+        ###########################
+        ### Autor: Eduardo Bono ###
+        ###  Fecha: 02/03/2022  ###
+        ###     Ejercicio 2     ###
+        ###########################
+'''
+
+# ANOTACIONES
+
+
+# VARIABLES
+N = 4
+A = 3.2
+c = "4"
+suma = N + A
+resta = A - N
+
+# FUNCIONES
+
+
+print("la variable N tiene un valor de: ", N)
+print("la variable A tiene un valor de: ", A)
+print('')
+print("La suma de las 2 variables es ", suma)
+print("La resta de las 2 variables es %.4f" % resta)
+print("La suma de las 2 variables es ", int(c))
---
+++ Ejercicio5.py
@@ -0,0 +1,40 @@
+'''
+        ###########################
+        ### Autor: Eduardo Bono ###
+        ###  Fecha: 02/03/2022  ###
+        ###     Ejercicio 5     ###
+        ###########################
+'''
+
+# ANOTACIONES
+
+
+# VARIABLES
+A = 2
+B = 4
+C = 6
+D = 8
+
+# FUNCIONES
+
+
+
+print("El valor inicial de las variables es:")
+print("A = ", A, ", B = ", B, ", C = ", C, ", D = ", D)
+print('')
+
+B = C
+print("B toma el valor de C, el valor de B ahora es: ", B)
+print('')
+
+C = A
+print("C toma el valor de A, el valor de C ahora es: ", C)
+print('')
+
+A = D
+print("A toma el valor de D, el valor de A ahora es: ", A)
+print('')
+
+D = B
+print("D toma el valor de B, el valor de D ahora es: ", D)
+
